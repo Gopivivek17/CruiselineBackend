4114633fbc8be9b77248bce8a2f169d063018d0c
Gopivivek17 <Shiramshetti.GopiVivek@cognizant.com>
2026-06-13 18:00:41 +0530

    cruiseline setup

---
+++ src/test/java/com/cts/CruiseLine/CruiseLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.CruiseLine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruiseLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
